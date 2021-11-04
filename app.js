c1b7dd7e674046267c40b7156422c4dc0b677b87
Holly Ford <holly_ford@hotmail.com>
2021-11-04 17:20:23 +0000

    added some styling

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const gameTwo = document.getElementById("gameTwo");
 const bttnReset = document.getElementById("bttnReset");
 const winLose = document.getElementById("winLose")
 
-
 scoreOne = 0;
 scoreTwo = 0;
 scoreOneP.textContent = scoreOne;
@@ -30,9 +29,9 @@ bttnReset.addEventListener("click", () => {
     scoreOneP.textContent = 0;
     scoreTwoP.textContent = 0;
     bttnRollOne.disabled = false;
-    bttnHoldOne.disabled = false; 
+    bttnHoldOne.disabled = false;
     bttnRollTwo.disabled = true;
-    bttnHoldTwo.disabled = true;  
+    bttnHoldTwo.disabled = true;
     diceOne.setAttribute("src", "./images/placeholderDice.png");
     diceTwo.setAttribute("src", "./images/placeholderDice.png");
     winLose.textContent = "Player 1's Turn"
@@ -40,98 +39,98 @@ bttnReset.addEventListener("click", () => {
 
 bttnHoldOne.addEventListener("click", () => {
     bttnRollTwo.disabled = false;
-    bttnHoldTwo.disabled = false; 
+    bttnHoldTwo.disabled = false;
     bttnRollOne.disabled = true;
-    bttnHoldOne.disabled = true; 
+    bttnHoldOne.disabled = true;
 })
 
 bttnHoldTwo.addEventListener("click", () => {
     bttnRollOne.disabled = false;
-    bttnHoldOne.disabled = false; 
+    bttnHoldOne.disabled = false;
     bttnRollTwo.disabled = true;
-    bttnHoldTwo.disabled = true; 
+    bttnHoldTwo.disabled = true;
 })
 
 bttnRollOne.addEventListener("click", () => {
-    let num = Math.ceil(Math.random() *6);
+    let num = Math.ceil(Math.random() * 6);
     if (num === 1) {
         diceOne.setAttribute("src", "./images/diceFace1.png");
         scoreOne = 0;
         scoreOneP.textContent = "Your Score: X";
-        winLose.textContent = "Player 1 has lost!"
-        bttnRollTwo.disabled = true;
-        bttnHoldTwo.disabled = true; 
+        winLose.textContent = "Player 1 has rolled a 1 - Player 2's turn!"
+        bttnRollTwo.disabled = false;
+        bttnHoldTwo.disabled = false;
         bttnRollOne.disabled = true;
-        bttnHoldOne.disabled = true; 
-      } else if (num === 2) {
+        bttnHoldOne.disabled = true;
+    } else if (num === 2) {
         diceOne.setAttribute("src", "./images/diceFace2.png");
         scoreOne += 2;
         scoreOneP.textContent = `Your Score: ${scoreOne}`;
-      } else if (num === 3) {
+    } else if (num === 3) {
         diceOne.setAttribute("src", "./images/diceFace3.png");
         scoreOne += 3;
         scoreOneP.textContent = `Your Score: ${scoreOne}`;
-      } else if (num === 4) {
+    } else if (num === 4) {
         diceOne.setAttribute("src", "./images/diceFace4.png");
         scoreOne += 4;
         scoreOneP.textContent = `Your Score: ${scoreOne}`;
-      } else if (num === 5) {
+    } else if (num === 5) {
         diceOne.setAttribute("src", "./images/diceFace5.png");
         scoreOne += 5;
         scoreOneP.textContent = `Your Score: ${scoreOne}`;
-      } else if (num === 6) {
+    } else if (num === 6) {
         diceOne.setAttribute("src", "./images/diceFace6.png");
         scoreOne += 6;
         scoreOneP.textContent = `Your Score: ${scoreOne}`;
-      }
-    
-      if (scoreOne >= 21) {
+    }
+
+    if (scoreOne >= 21) {
         winLose.textContent = "Player 1 has won!"
         bttnRollTwo.disabled = true;
-        bttnHoldTwo.disabled = true; 
+        bttnHoldTwo.disabled = true;
         bttnRollOne.disabled = true;
-        bttnHoldOne.disabled = true; 
-      }
+        bttnHoldOne.disabled = true;
+    }
 })
 
 bttnRollTwo.addEventListener("click", () => {
-    let num = Math.ceil(Math.random() *6);
+    let num = Math.ceil(Math.random() * 6);
     if (num === 1) {
         diceTwo.setAttribute("src", "./images/diceFace1.png");
         scoreTwo = 0;
         scoreTwoP.textContent = "Your Score: X";
-        winLose.textContent = "Player 2 has lost!"
-        bttnRollOne.disabled = true;
-        bttnHoldOne.disabled = true; 
+        winLose.textContent = "Player 2 has rolled a 1 - Player 1's turn!"
+        bttnRollOne.disabled = false;
+        bttnHoldOne.disabled = false;
         bttnRollTwo.disabled = true;
-        bttnHoldTwo.disabled = true; 
-      } else if (num === 2) {
+        bttnHoldTwo.disabled = true;
+    } else if (num === 2) {
         diceTwo.setAttribute("src", "./images/diceFace2.png");
         scoreTwo += 2;
         scoreTwoP.textContent = `Your Score: ${scoreTwo}`;
-      } else if (num === 3) {
+    } else if (num === 3) {
         diceTwo.setAttribute("src", "./images/diceFace3.png");
         scoreTwo += 3;
         scoreTwoP.textContent = `Your Score: ${scoreTwo}`;
-      } else if (num === 4) {
+    } else if (num === 4) {
         diceTwo.setAttribute("src", "./images/diceFace4.png");
         scoreOne += 4;
         scoreTwoP.textContent = `Your Score: ${scoreTwo}`;
-      } else if (num === 5) {
+    } else if (num === 5) {
         diceTwo.setAttribute("src", "./images/diceFace5.png");
         scoreTwo += 5;
         scoreTwoP.textContent = `Your Score: ${scoreTwo}`;
-      } else if (num === 6) {
+    } else if (num === 6) {
         diceTwo.setAttribute("src", "./images/diceFace6.png");
         scoreTwo += 6;
         scoreTwoP.textContent = `Your Score: ${scoreTwo}`;
-      }
-    
-      if (scoreTwo >= 21) {
+    }
+
+    if (scoreTwo >= 21) {
         winLose.textContent = "Player 2 has won!"
         bttnRollTwo.disabled = true;
-        bttnHoldTwo.disabled = true; 
+        bttnHoldTwo.disabled = true;
         bttnRollOne.disabled = true;
-        bttnHoldOne.disabled = true; 
-      }
+        bttnHoldOne.disabled = true;
+    }
 })
